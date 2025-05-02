a8c047cf595210276195c4067467811cd4eb1c4c
Tereza Matousova <tereza.matousova@gmail.com>
2025-05-02 14:50:55 +0200

    Určení vítěze nebo remíza

--- hra.js
+++ hra.js
@@ -1,8 +1,9 @@
+import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
+
 let currentPlayer = 'circle';
 
 const gameField = document.querySelectorAll('.hra__pole button');
-
-const playerIndicator = document.querySelector('.hra__kruh'); // Změna symbolu
+const playerIndicator = document.querySelector('.hra__kruh');
 const restartLink = document.querySelector('.hra__restart');
 
 /* Křížky a kolečka */
@@ -13,8 +14,8 @@ const changeToCircle = (event) => {
 const changeToCross = (event) => {
   event.target.classList.add('board__field--cross');
 };
-/* Změna symbolů dle hrajícího */
 
+/* Změna symbolů dle hrajícího */
 const updatePlayerIndicator = () => {
   playerIndicator.classList.remove('hra__kruh--circle', 'hra__kruh--cross');
   if (currentPlayer === 'circle') {
@@ -24,13 +25,22 @@ const updatePlayerIndicator = () => {
   }
 };
 
-/* Hrací pole */
+/* Zablokování hry po výhře nebo remíze */
+const disableGame = () => {
+  gameField.forEach((field) => {
+    field.disabled = true;
+  });
+};
 
+/* Kliknutí na jedno pole */
 const handleClick = (event) => {
   const field = event.target;
 
-  if (field.classList.contains('board__field--circle')) return;
-  if (field.classList.contains('board__field--cross')) return;
+  if (
+    field.classList.contains('board__field--circle') ||
+    field.classList.contains('board__field--cross')
+  )
+    return;
 
   if (currentPlayer === 'circle') {
     changeToCircle(event);
@@ -41,10 +51,39 @@ const handleClick = (event) => {
   }
 
   updatePlayerIndicator();
+
+  /* Určení výherce */
+  const herniPole = Array.from(gameField).map((field) => {
+    if (field.classList.contains('board__field--cross')) return 'x';
+    if (field.classList.contains('board__field--circle')) return 'o';
+    return '_';
+  });
+
+  const vitez = findWinner(herniPole);
+
+  /* Výhra nebo remíza */
+  if (vitez === 'x') {
+    disableGame();
+    setTimeout(() => {
+      alert('Vyhrál křížek!');
+      location.reload();
+    }, 100);
+  } else if (vitez === 'o') {
+    disableGame();
+    setTimeout(() => {
+      alert('Vyhrálo kolečko!');
+      location.reload();
+    }, 100);
+  } else if (vitez === 'tie') {
+    disableGame();
+    setTimeout(() => {
+      alert('Remíza! Nikdo nevyhrál.');
+      location.reload();
+    }, 100);
+  }
 };
 
 /* Kliknutí na restart */
-
 const whenRestartBtn = (event) => {
   const confirmed = confirm('Opravdu chceš začít znovu?');
   if (!confirmed) {
